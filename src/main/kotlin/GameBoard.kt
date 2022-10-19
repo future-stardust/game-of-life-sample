7f0e4b9aacfd08b40721826eb1c6df5d2289e3ed
Michael Pustovit <mykhailopu@wix.com>
2022-10-19 11:18:18 +0300

    Add game IO and main fun implementation

--- src/main/kotlin/GameBoard.kt
+++ src/main/kotlin/GameBoard.kt
@@ -116,11 +116,3 @@ private data class GameBoardImpl(
     return count
   }
 }
-
-fun playGame(board: GameBoard): GameBoard {
-  var brd = board
-  for (i in 0 until board.leftGenerationsCount()) {
-    brd = brd.makeStep()
-  }
-  return brd
-}
